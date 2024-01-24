3a4a688ef09bce31f427fcff83394fd3b52bfe7e
wieczorekkevin <wieczorek.kevin.m@gmail.com>
2024-01-24 17:30:22 -0500

    Implemented incrementing button

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.simplecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textview = findViewById<TextView>(R.id.textView)
+
+        var counter = 0
+
+        button.setOnClickListener {
+            //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+
+            counter++
+            textview.text = counter.toString()
+        }
     }
 }
