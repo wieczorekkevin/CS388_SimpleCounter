d7021b93debf274529ef143eff894a63d7cb23b7
wieczorekkevin <wieczorek.kevin.m@gmail.com>
2024-01-24 18:03:51 -0500

    Final Lab 1 submission

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simplecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -13,14 +14,26 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
         val textview = findViewById<TextView>(R.id.textView)
+        val upgradeButton = findViewById<Button>(R.id.upgradeButton)
 
         var counter = 0
+        var incrementer = 1
 
         button.setOnClickListener {
             //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
 
-            counter++
+            counter += incrementer
             textview.text = counter.toString()
+
+            if (counter >= 100 && incrementer == 1) {
+                upgradeButton.visibility = View.VISIBLE
+            }
+        }
+
+        upgradeButton.setOnClickListener {
+            incrementer += 1
+            upgradeButton.visibility = View.INVISIBLE
+            button.setText("ADD 2")
         }
     }
 }
